da7550e358fc8e7d520e5618f6fd1fd4bc69ced5
KaungPyaeSoneAung2 <kaungpyae.kpp16@gmail.com>
2023-05-31 20:10:08 +0630

    Hw3 const Strings added

---
+++ lib/constants/hw3/strings.dart
@@ -0,0 +1,8 @@
+const ksYourName = "Your Name";
+const ksYourEmail = "Your Email";
+const ksPW = "Password";
+const ksRpPW = "Repeat your password";
+const ksTOS1 = "I agree all statements in ";
+const ksTOS2 = "Terms of service";
+const ksHaveAcc1= "Have already an Account ? ";
+const ksHaveAcc2= "Login Here";
